6c0bfb7813df7d72286e952d323af0dabf7c4c24
scott-david-walker <scott.david.walker@hotmail.com>
2024-06-03 16:47:40 +0200

    feat: add conflict exception

---
+++ src/FreeFoxSoftware.Core/Exceptions/ConflictException.cs
@@ -0,0 +1,9 @@
+namespace FreeFoxSoftware.Core.Exceptions;
+
+public class ConflictException : DomainException
+{
+    public ConflictException(string identifier) : base($"'{identifier}' already exists.")
+    {
+        StatusCode = 409;
+    }
+}
